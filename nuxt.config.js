9b96ed30a3bc67997e6eb32b0d4b379c818f45c4
Hayden Lugo <hayden@lugowebdevelopment.com>
2021-11-05 00:53:30 +0100

    redux resize text

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,9 +43,6 @@ export default {
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
 		'~/plugins/vee-validate.js',
-		{ src: '~/plugins/scale-text.js'
-			// , mode: 'client', ssr: false 
-		}
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
